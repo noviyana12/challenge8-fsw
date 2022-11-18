e576de3e0264922e8ee25f1e0b100a879ffdf066
Noviyana <lingnoviyana123@gmail.com>
2022-11-18 19:57:58 +0700

    new update

--- app/models/index.js
+++ app/models/index.js
@@ -15,7 +15,12 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize('postgresql://postgres:QkLueXl5hVwDArxxMLgp@containers-us-west-114.railway.app:7072/railway', {
+    define: {
+      timestamps: false,
+    },
+  });
 }
 
 fs
