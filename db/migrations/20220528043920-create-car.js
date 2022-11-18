063f3b8c0f52ce2ee8c03bde9aa8a839d0377452
Noviyana <lingnoviyana123@gmail.com>
2022-11-18 22:03:27 +0700

    up

--- db/migrations/20220528043920-create-car.js
+++ db/migrations/20220528043920-create-car.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Cars', {
+    await queryInterface.createTable('cars', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -35,6 +35,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Cars');
+    await queryInterface.dropTable('cars');
   },
 };
